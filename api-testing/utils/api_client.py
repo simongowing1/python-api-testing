1ce651015b921d78d80933333918934cd1bc5dad
dev-sigo <dev@platformplatform.com>
2024-09-25 15:04:54 +0200

    feat: add post, get, delete methods to api_client

--- api-testing/utils/api_client.py
+++ api-testing/utils/api_client.py
@@ -3,3 +3,12 @@ import requests
 class APIClient:
     def __init__(self):
         self.base_url = "https://reqres.in"
+    
+    def post(self, endpoint, data):
+        return requests.post(f"{self.base_url}{endpoint}", json=data)
+
+    def get(self, endpoint):
+        return requests.get(f"{self.base_url}{endpoint}")
+
+    def delete(self, endpoint):
+        return requests.delete(f"{self.base_url}{endpoint}")
