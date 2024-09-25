495f8544a843a254d84fff5bac47f5814b9aca77
dev-sigo <dev@platformplatform.com>
2024-09-25 17:09:15 +0200

    feat: add create user tests

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -1,4 +1,24 @@
 import pytest
+from api_testing.utils.api_client import APIClient
 
-def test_create_user():
-    pass
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+@pytest.fixture
+def dummy_user_details():
+    return {
+    "name": "morpheus",
+    "job": "leader"
+}
+
+def test_create_user_success(api_client, dummy_user_details):
+    response = api_client.post("/api/users", dummy_user_details)
+    assert response.status_code == 201, f"Request failed with status code {response.status_code}: {response.text}"
+    print("response:", response.json())
+
+# FAILS - CREATES USER WITH JUST "ID" AND "CREATEDATE"
+def test_create_user_failure_empty_body(api_client):
+    response = api_client.post("/api/users", {})
+    assert response.status_code == 400, f"Request failed with status code {response.status_code}: {response.text}"
+    print("response:", response.json())
