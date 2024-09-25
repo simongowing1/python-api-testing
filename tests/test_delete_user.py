cf13366f491a75f51600dc650c2628e8ba6757f4
dev-sigo <dev@platformplatform.com>
2024-09-25 20:24:05 +0200

    feat: add test_delete_specific_user_204

--- tests/test_delete_user.py
+++ tests/test_delete_user.py
@@ -1,4 +1,30 @@
 import pytest
 
-def test_delete_user():
-    pass
+from api_testing.utils.api_client import APIClient
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+@pytest.fixture
+def user_fixture():
+    return {
+        "data": {
+            "id": 2,
+            "email": "janet.weaver@reqres.in",
+            "first_name": "Janet",
+            "last_name": "Weaver",
+            "avatar": "https://reqres.in/img/faces/2-image.jpg"
+            },
+        "support": {
+            "url": "https://reqres.in/#support-heading",
+            "text": "To keep ReqRes free, contributions towards server costs are appreciated!"
+            }
+            }
+    
+def test_delete_specific_user_204(api_client, user_fixture):
+    id = user_fixture['data']['id']
+    response = api_client.delete(f"/api/users/{id}")
+    assert response.status_code == 204, f"Test failed. Response status code {response.status_code}: {response.text}"
+
+    assert response.text == ''
