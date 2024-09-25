f0b0ce370e61f754c6419801f5d198bbd0499454
dev-sigo <dev@platformplatform.com>
2024-09-25 20:11:40 +0200

    refactor: test_get_specific_user to check type of json_response

--- tests/test_get_specific_user.py
+++ tests/test_get_specific_user.py
@@ -26,8 +26,12 @@ def test_get_specific_user_success(api_client, user_fixture):
     id = user_fixture['data']['id']
     response = api_client.get(f"/api/users/{id}")
     assert response.status_code == 200, f"Request failed with status code {response.status_code}: {response.text}"
-    assert response.json() == user_fixture
+
+    json_response = response.json()
+    assert isinstance(json_response, dict), f"Response JSON is not type: 'dictionary'."
+
+    assert json_response == user_fixture
 
 def test_get_specific_user_failure(api_client, user_fixture):
     response = api_client.get("/api/users/foo")
-    assert response.status_code == 404, f"Request failed with status code {response.status_code}: {response.text}"
+    assert response.status_code == 404, f"Test failed. Request status code {response.status_code}: {response.text}"
