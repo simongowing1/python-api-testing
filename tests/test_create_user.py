1d692f0c9630d0cf485bc9a0e6305a8b9c89174b
dev-sigo <dev@platformplatform.com>
2024-09-25 20:44:29 +0200

    refactor: add further checks to ensure response has "expected JSON attributes"

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -11,7 +11,15 @@ def dummy_user_details():
 def test_create_user_success(api_client, dummy_user_details):
     response = api_client.post("/api/users", dummy_user_details)
     assert response.status_code == 201, f"Test failed. Response status code is {response.status_code}: {response.text}"
-    print("response:", response.json())
+
+    json_response = response.json()
+    assert 'name' in json_response, "Test failed. Response JSON does not contain key: 'name'."
+    assert json_response['name'] == dummy_user_details['name'], f"Test failed. Response does not match expected value"
+
+    assert 'job' in json_response, "Test failed. Response JSON does not contain key: 'job'."
+    assert json_response['job'] == dummy_user_details['job'], f"Test failed. Response does not match expected value"
+   
+    print("response:", json_response)
 
 # FAILS - CREATES USER WITH JUST "ID" AND "CREATEDATE"
 def test_create_user_failure_empty_body(api_client):
