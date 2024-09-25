3cd81317b0eab79f9c79b15e255ac71938dd4c40
dev-sigo <dev@platformplatform.com>
2024-09-25 15:46:50 +0200

    feat: create new login tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,4 +1,37 @@
 import pytest
+from api_testing.utils.api_client import APIClient
 
-def test_login():
-    pass
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+@pytest.fixture
+def correct_login_details():
+    return {
+        "email": "eve.holt@reqres.in",
+        "password": "cityslicka"
+        }
+
+@pytest.fixture
+def incorrect_login_details():
+    return {
+        "email": "name@email.com",
+        "password": "password"
+        }
+
+def test_login_success_correct_details(api_client, correct_login_details):
+    response = api_client.post("/api/login", correct_login_details)
+    assert response.status_code == 200, f"Failed with status code {response.status_code}: {response.text}"
+
+def test_login_success_incorrect_details(api_client, incorrect_login_details):
+    response = api_client.post("/api/login", incorrect_login_details)
+    assert response.status_code == 400, f"Failed with status code {response.status_code}: {response.text}"
+
+def test_login_failure_no_password(api_client, correct_login_details):
+    response = api_client.post("/api/login", {"email": correct_login_details["email"]})
+    assert response.status_code == 400, f"Failed with status code {response.status_code}: {response.text}"
+
+def test_login_failure_no_email(api_client, correct_login_details):
+    response = api_client.post("/api/login", {"password": correct_login_details["password"]})
+    assert response.status_code == 400, f"Failed with status code {response.status_code}: {response.text}"
+
