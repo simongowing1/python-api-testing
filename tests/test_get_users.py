2e645802d77c10187666a360800f7e196b764586
dev-sigo <dev@platformplatform.com>
2024-09-25 20:00:34 +0200

    feat: add test get users

--- tests/test_get_users.py
+++ tests/test_get_users.py
@@ -1,4 +1,15 @@
 import pytest
+from api_testing.utils.api_client import APIClient
 
-def test_get_users():
-    pass
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+def test_get_users(api_client):
+    response = api_client.get("/api/users")
+    assert response.status_code == 200, f"Request failed with status code {response.status_code}: {response.text}"
+
+def test_total_users(api_client):
+    response = api_client.get("/api/users")
+    total_users = response.json()['total']
+    assert total_users > 0, f"Test failed. There are no users."
