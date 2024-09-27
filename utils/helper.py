02f1fa87f4fb1a1bd98c8e824af711db81f06d8f
dev-sigo <dev@platformplatform.com>
2024-09-27 13:30:05 +0200

    refactor: improve assertion_error_message_standard

--- utils/helper.py
+++ utils/helper.py
@@ -1,2 +1,2 @@
 def assertion_error_message_standard(response):
-    return f"Test failed. Response status code is {response.status_code}: {response.text}"
+    return f"\n[ERROR] Test failed. \n[ERROR] Status Code: {response.status_code}\n[ERROR] Response: {response.text}"
